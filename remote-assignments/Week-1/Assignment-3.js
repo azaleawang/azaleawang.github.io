ebbd0799b23f94b5908bb3d60eb7139426362d1a
azalea <azaleawang4@gmail.com>
2023-09-10 17:35:06 +0800

    Use filter to improve readability

--- remote-assignments/Week-1/Assignment-3.js
+++ remote-assignments/Week-1/Assignment-3.js
@@ -1,18 +1,23 @@
 function countAandB(input) {
-    // your code here
-    return input.reduce(
-        (count, e) => count + (['a', 'b'].includes(e.toLowerCase()) ? 1 : 0), 
-        0)
+  // filter Solution
+  return input
+    .filter((e) => ['a', 'b'].includes(e.toLowerCase()))
+    .length
+
+  // reduce Solution
+  // return input.reduce(
+  //     (count, e) => count + (['a', 'b'].includes(e.toLowerCase()) ? 1 : 0),
+  //     0)
 }
 
 function toNumber(input) {
-    // your code here
-    return input.map((e) => e.toLowerCase().charCodeAt() - 'a'.charCodeAt() + 1)
+  // your code here
+  return input.map((e) => e.toLowerCase().charCodeAt() - "a".charCodeAt() + 1);
 }
 
-let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+let input1 = ["a", "b", "c", "a", "c", "a", "c"];
 console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
 console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
-let input2 = ['e', 'd', 'c', 'd', 'e'];
+let input2 = ["e", "d", "c", "d", "e"];
 console.log(countAandB(input2)); // should print 0
 console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
