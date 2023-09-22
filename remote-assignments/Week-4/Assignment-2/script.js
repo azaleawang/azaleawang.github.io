e324a3ebe8d9b9d28899490e2167f2f46a2fa8d0
azalea <azaleawang4@gmail.com>
2023-09-23 03:08:37 +0800

    Simplify and tidy up codes

--- remote-assignments/Week-4/Assignment-2/script.js
+++ remote-assignments/Week-4/Assignment-2/script.js
@@ -4,45 +4,46 @@ response data on the page. You may render UI with any style.
  */
 
 // handling click event
-const clickHandler = () => {
+const clickHandler = (e) => {
   ajax(
     "https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products",
     function (response) {
       render(response);
     }
-  );
+  ).finally(() => e.target.remove());
 };
 
 // get data by AJAX fetch
 function ajax(src, callback) {
   // your code here
-  fetch(src)
+  return fetch(src)
     .then((response) => response.json())
     .then((data) => callback(data))
-    .then(() => console.log("callback done"));
+    .then(() => console.log("callback done"))
+    .catch((error) => {
+      console.error("Error in AJAX request:", error);
+    });
 }
 
 // render data on page
 function render(data) {
   // your code here
-  const contentDiv = document.createElement("div");
+  const contentDiv = document.createElement("ul");
   contentDiv.className = "render-json";
-  for (const prod of data) {
-    let prodOl = document.createElement("ol");
-    let name = document.createElement("h3");
-    name.textContent = prod.name;
-    let descpt = document.createElement("p");
-    descpt.innerHTML = `<em>${prod.description}</em>`;
-
-    let price = document.createElement("p");
-    price.innerHTML = `<p>$ ${prod.price}</p>`;
-    prodOl.appendChild(name);
-    prodOl.appendChild(descpt);
-    prodOl.appendChild(price);
-    contentDiv.insertAdjacentElement("beforeend", prodOl);
-  }
+  data.forEach((prod) => {
+    let prodLi = document.createElement("li");
+    prodLi.innerHTML = `
+      <h3>${prod.name}</h3>
+      <p><em>${prod.description}</em></p>
+      <p>$ ${prod.price}</p>
+      <br/>
+      `;
+    contentDiv.insertAdjacentElement("beforeend", prodLi);
+  });
   document.querySelector(".main").appendChild(contentDiv);
 }
 
 // register click event listener
-document.querySelector(".fetchBtn").addEventListener("click", clickHandler);
+document
+  .querySelector(".fetchBtn")
+  .addEventListener("click", (e) => clickHandler(e));
